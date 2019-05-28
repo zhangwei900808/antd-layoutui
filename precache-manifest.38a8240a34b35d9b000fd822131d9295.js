3036bb284ac5607636d06209eb01ed14facfaf5a
张威 <zw900808@gmail.com>
2019-05-28 11:50:40 +0800

    Updates

---
+++ precache-manifest.38a8240a34b35d9b000fd822131d9295.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31575bb61bb09e3294171dd84c8aed8e",
+    "url": "/antd-layoutui/index.html"
+  },
+  {
+    "revision": "8f1ae3b3f3cb9f09ae19",
+    "url": "/antd-layoutui/static/css/2.08d3cf41.chunk.css"
+  },
+  {
+    "revision": "ce1d26ae4909016f3858",
+    "url": "/antd-layoutui/static/css/3.a4f8a84e.chunk.css"
+  },
+  {
+    "revision": "42352b9a0b7bf2ccbece",
+    "url": "/antd-layoutui/static/css/4.fc248f15.chunk.css"
+  },
+  {
+    "revision": "62c659ffdde5aaa8c8e3",
+    "url": "/antd-layoutui/static/css/main.7229a0aa.chunk.css"
+  },
+  {
+    "revision": "8f1ae3b3f3cb9f09ae19",
+    "url": "/antd-layoutui/static/js/2.176473da.chunk.js"
+  },
+  {
+    "revision": "ce1d26ae4909016f3858",
+    "url": "/antd-layoutui/static/js/3.d889c032.chunk.js"
+  },
+  {
+    "revision": "42352b9a0b7bf2ccbece",
+    "url": "/antd-layoutui/static/js/4.38a5a9bd.chunk.js"
+  },
+  {
+    "revision": "62c659ffdde5aaa8c8e3",
+    "url": "/antd-layoutui/static/js/main.8bdb3fef.chunk.js"
+  },
+  {
+    "revision": "8ccef46f926686786724",
+    "url": "/antd-layoutui/static/js/runtime~main.2661e055.js"
+  },
+  {
+    "revision": "7aa7a68a7996296683e2c5d6e313afc3",
+    "url": "/antd-layoutui/static/media/awbeci.7aa7a68a.png"
+  },
+  {
+    "revision": "298174b5d21dd23575abfa4140719554",
+    "url": "/antd-layoutui/static/media/bgLogo.298174b5.png"
+  }
+]);

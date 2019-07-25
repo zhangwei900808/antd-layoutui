31a2c3fd4783c838feceefa1c696d1ad595f906c
张威 <zw900808@gmail.com>
2019-07-25 16:47:07 +0800

    优化页面

--- src/config/base.conf.js
+++ src/config/base.conf.js
@@ -1,10 +1,10 @@
 export default {
-  company: "海云数据",
+  company: "Awbeci",
   title: "后台管理系统平台",
   subTitle: "后台管理系统平台",
-  enTitle: "Hiynn Manage platform",
-  copyright: "Copyright © 2019 Hiynn All Rights Reserved.",
-  logo: require("../assets/image/hiy_logo.png"),
+  enTitle: "Awbeci Manage platform",
+  copyright: "Copyright © 2019 Awbeci All Rights Reserved.",
+  logo: require("../assets/image/awbeci.png"),
   host: "http://10.0.91.153",
   port: "8088", //8088
   persist: "root"
